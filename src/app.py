9df4583fe874f3d06281f603bcdb5a0f957233ca
debsubdavis <95187410+debsubdavis@users.noreply.github.com>
2022-08-02 14:01:46 -0600

    changes to xml_search_terms.json

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from socket import SO_VM_SOCKETS_BUFFER_MIN_SIZE
 import numpy as np
 import pandas as pd
+import json
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 
@@ -11,12 +12,17 @@ from spacy.matcher import PhraseMatcher
 
 listing_xml = '../app/4680Whitely_MLSListing'
 vocab_csv = "../app/Vocab2.csv"
+xml_search_json = '../app/xml_search_terms.json'
+
+with open(listing_xml) as fp:
+    soup = BeautifulSoup(fp, 'lxml-xml')
+print(soup.prettify())
 
 
 def load_vocab(filepath):
     # vocab = pd.read_csv("../app/Vocab.csv")
     vocab = pd.read_csv(filepath)
-    print(vocab)
+    # print(vocab)
     return vocab
 
 
@@ -24,19 +30,23 @@ def get_unique_categories(vocab):
     return vocab.Category.unique()
 
 
-def phrase_matcher(categories, vocab, doc):
-    for category in categories:
-        matcher = PhraseMatcher(nlp.vocab, attr='LEMMA')
-        phrase_list = vocab.loc[vocab['Category'] == category].Phrase
-        # print(phrase_list)
-        phrase_patterns = [nlp(text) for text in phrase_list]
-        # print(phrase_patterns)
-        matcher.add('TerminologyList', phrase_patterns)
-        matches = matcher(public_remarks_doc)
-        print(matches)
-        for match_id, start, end in matches:
-            span = doc[start:end]
-            print(span.text)
+def phrase_matcher(category, vocab, doc):
+  
+    matcher = PhraseMatcher(nlp.vocab, attr='LEMMA')
+    # print("****Printing vocab.Category", vocab['Category'] == category)
+    phrase_list = vocab.loc[vocab['Category'] == category].Phrase
+    # print("*****PRINTING PHRASE LIST*****")
+    # for t in phrase_list:
+    #     print(t)
+    phrase_patterns = [nlp(text) for text in phrase_list]
+    print("\nPHRASE PATTERNS FROM VOCAB.CSV: ", phrase_patterns, "\nFOR CATEGORY: ", category)
+    matcher.add('TerminologyList', phrase_patterns)
+    matches = matcher(doc)
+    # print(matches)
+    # for match_id, start, end in matches:
+    #     span = doc[start:end]
+    #     print(span.text)
+    return matches
 
 def get_public_remarks():
     tree = ET.parse(listing_xml)
@@ -76,24 +86,75 @@ def search_room_types_etree():
         room_types.append(element.tag)
     return room_types
 
-def search_room_types_beautifulsoup():
-    print("beautiful soup")
-    with open(listing_xml) as fp:
-        soup = BeautifulSoup(fp, 'lxml-xml')
-    print(soup.prettify())
-    interior_features = soup.find_all("InteriorFeatures")
+def search_beautifulsoup(search_phrase):
+    # print("beautiful soup")
+    # print("search_phrase: ", search_phrase)
+    # with open(listing_xml) as fp:
+    #     soup = BeautifulSoup(fp, 'lxml-xml')
+    # print(soup.prettify())
+    search_result = soup.find_all(search_phrase)
+
+    for s in search_result:
+        # print("s in search_result", s.get_text())
+        return s.get_text()
+
+class Search:
+    def __init__(self, name, xml_search_terms, categories):
+        self.name = name
+        self.xml_search_terms = xml_search_terms
+        self.categories = categories
+        self.phrases = [] # based on parsing the MLS Listing XML for the search terms
 
 
-phrase_to_process = get_public_remarks()
-print("\n\nPHRASE TO PROCESS: \n", phrase_to_process, "\n\n")
-public_remarks_doc = nlp(phrase_to_process)
 
-# rooms = search_room_types_etree()
-rooms = search_room_types_beautifulsoup()
-print(rooms)
+# phrase_to_process = get_public_remarks()
+# print("\n\nPHRASE TO PROCESS: \n", phrase_to_process, "\n\n")
+# public_remarks_doc = nlp(phrase_to_process)
 
-vocab = load_vocab(vocab_csv)
-categories = get_unique_categories(vocab)
-print(categories)
+# # rooms = search_room_types_etree()
+# rooms = search_beautifulsoup()
+# print(rooms)
 
-phrase_matcher(categories, vocab, public_remarks_doc)
+# vocab = load_vocab(vocab_csv)
+# categories = get_unique_categories(vocab)
+# print(categories)
+
+# phrase_matcher(categories, vocab, public_remarks_doc)
+
+
+with open(xml_search_json , 'r') as f:
+    data = json.load(f)
+
+f.close()
+xml_searches = []
+
+for i in data:
+    name = i
+    # print(name)
+    xml_search_terms = data[name]['xml_search_terms']
+    # print(search_terms)
+    categories = data[name]['categories']
+    # print(categories)
+    search = Search(name, xml_search_terms, categories)
+    # print(s.name, "-->", s.search_terms, "-->", s.categories)
+    xml_searches.append(search)
+
+for s in xml_searches:
+    # print(s.name, "-->", s.search_terms, "-->", s.categories)
+
+    for term in s.xml_search_terms:
+        print("\n\n***FIND IN XML FOR MLS LISTING: ", term)
+        doc = nlp(search_beautifulsoup(term))
+        print("\nTEXT FROM MLS LISTING: \n", doc)
+
+        # print("\n\nStarting Category\n")
+        
+        for category in s.categories:
+            print("\nCATEGORY FOR PHRASE MATCHER: ", category)
+            v = load_vocab(vocab_csv)
+            matches = phrase_matcher(category, v, doc)
+            for match_id, start, end in matches:
+                print("\nMATCH: ")
+                span = doc[start:end]
+                print(span.text)
+
