2dd6547551ecf6af89aaa9dbaf7159faa234112c
debsubdavis <95187410+debsubdavis@users.noreply.github.com>
2022-08-08 09:10:26 -0700

    cleaned up code to use functions

--- src/app.py
+++ src/app.py
@@ -11,13 +11,20 @@ nlp = spacy.load("en_core_web_sm")
 from spacy.matcher import PhraseMatcher
 
 listing_xml = '../app/4680Whitely_MLSListing'
-vocab_csv = "../app/Vocab2.csv"
+vocab_csv = "../app/Vocab.csv"
 xml_search_json = '../app/xml_search_terms.json'
 
-with open(listing_xml) as fp:
-    soup = BeautifulSoup(fp, 'lxml-xml')
-print(soup.prettify())
 
+class Search:
+    def __init__(self, name, xml_search_terms, categories):
+        self.name = name
+        self.xml_search_terms = xml_search_terms
+        self.categories = categories
+        self.phrases = [] # based on parsing the MLS Listing XML for the search terms
+
+class Listing:
+	def __init__(self, full_street_address, city, county, postal_code, latitude, longitude):
+		self.full_street_address = full_street_address
 
 def load_vocab(filepath):
     # vocab = pd.read_csv("../app/Vocab.csv")
@@ -48,50 +55,15 @@ def phrase_matcher(category, vocab, doc):
     #     print(span.text)
     return matches
 
-def get_public_remarks():
-    tree = ET.parse(listing_xml)
-    root = tree.getroot()
 
-    phrase_to_process = "initial"
+def make_the_soup():
+	with open(listing_xml) as fp:
+		soup = BeautifulSoup(fp, 'lxml-xml')
+		# print(soup.prettify())
+	fp.close()
+	return soup
 
-    for elem in root.iter('BrightAll'):
-        for e in elem.iter('PublicRemarks'):
-            # print(e.text)
-            phrase_to_process = e.text.lower()
-    return phrase_to_process
-
-def search_room_types_etree():
-    tree = ET.parse(listing_xml)
-    root = tree.getroot()
-
-    room_types = []
-
-    # for element in root.findall('BrightAll/*Remarks'):
-    #     print(element.tag)
-    #     room_types.append(element.tag)
-    #     for e in element.iter():
-    #         print(e.tag)
-
-    # test = list(root.iterfind('*Remarks'))
-    # for i in test:
-    #     print(i)
-
-    # if the current element is BrightAll, keep iterating 
-
-    all_descendants = list(root.iter())
-    print(all_descendants)
-
-    for element in tree.findall('*/*Remarks'):
-        print(element.tag)
-        room_types.append(element.tag)
-    return room_types
-
-def search_beautifulsoup(search_phrase):
-    # print("beautiful soup")
-    # print("search_phrase: ", search_phrase)
-    # with open(listing_xml) as fp:
-    #     soup = BeautifulSoup(fp, 'lxml-xml')
-    # print(soup.prettify())
+def search_beautifulsoup(search_phrase, soup):
     search_result = soup.find_all(search_phrase)
 
     for s in search_result:
@@ -99,54 +71,20 @@ def search_beautifulsoup(search_phrase):
         return s.get_text()
 
 
-def get_listing_address(listing_obj):
+def get_listing_address(soup):
 
-	full_street_address = search_beautifulsoup("FullStreetAddress")
-	city = search_beautifulsoup("City")
-	county = search_beautifulsoup("County")
-	postal_code = search_beautifulsoup("PostalCode")
-	postal_code_plus4 = search_beautifulsoup("PostalCodePlus4")
-	latitude = search_beautifulsoup("Latitude")
-	longitude = search_beautifulsoup("Longitude")
+	full_street_address = search_beautifulsoup("FullStreetAddress", soup)
+	city = search_beautifulsoup("City", soup)
+	county = search_beautifulsoup("County", soup)
+	postal_code = search_beautifulsoup("PostalCode", soup)
+	postal_code_plus4 = search_beautifulsoup("PostalCodePlus4", soup)
+	latitude = search_beautifulsoup("Latitude", soup)
+	longitude = search_beautifulsoup("Longitude", soup)
 
 	print(full_street_address, "-->", city, "-->", county, "-->", postal_code, "-->", postal_code_plus4, "-->", latitude, "-->", longitude )
 
-# LocationAddress
-# 	City
-# 	County
-# 	FullStreetAddress
-# 	PostalCode
-# 	PostalCodePlus4
-
-class Search:
-    def __init__(self, name, xml_search_terms, categories):
-        self.name = name
-        self.xml_search_terms = xml_search_terms
-        self.categories = categories
-        self.phrases = [] # based on parsing the MLS Listing XML for the search terms
-
-class Listing:
-	def __init__(self, full_street_address, city, county, postal_code, latitude, longitude):
-		self.full_street_address = full_street_address
-
-
-
-# phrase_to_process = get_public_remarks()
-# print("\n\nPHRASE TO PROCESS: \n", phrase_to_process, "\n\n")
-# public_remarks_doc = nlp(phrase_to_process)
-
-# # rooms = search_room_types_etree()
-# rooms = search_beautifulsoup()
-# print(rooms)
-
-# vocab = load_vocab(vocab_csv)
-# categories = get_unique_categories(vocab)
-# print(categories)
-
-# phrase_matcher(categories, vocab, public_remarks_doc)
-
-def xml_searches():
-	with open(xml_search_json , 'r') as f:
+def xml_searches(xml_search_filepath, vocab, soup):
+	with open(xml_search_filepath, 'r') as f:
 		data = json.load(f)
 
 	f.close()
@@ -168,19 +106,26 @@ def xml_searches():
 
 		for term in s.xml_search_terms:
 			print("\n\n***FIND IN XML FOR MLS LISTING: ", term)
-			doc = nlp(search_beautifulsoup(term))
+			doc = nlp(search_beautifulsoup(term, soup))
 			print("\nTEXT FROM MLS LISTING: \n", doc)
 
 			# print("\n\nStarting Category\n")
 			
 			for category in s.categories:
 				print("\nCATEGORY FOR PHRASE MATCHER: ", category)
-				v = load_vocab(vocab_csv)
-				matches = phrase_matcher(category, v, doc)
+				# v = load_vocab(vocab_csv)
+				matches = phrase_matcher(category, vocab, doc)
 				for match_id, start, end in matches:
 					print("\nMATCH: ")
 					span = doc[start:end]
 					print(span.text)
 
-get_listing_address()
+
+vocab = load_vocab(vocab_csv)
+categories = get_unique_categories(vocab)
+
+soup = make_the_soup()
+get_listing_address(soup)
+
+xml_searches(xml_search_json, vocab, soup)
 
